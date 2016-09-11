138ad31b0294ecd1bef5658de3c4ccc5ad0b9ee3
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-09-11 19:46:47 +0300

    preety print stress results

--- app/client/main.go
+++ app/client/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	vegeta "github.com/tsenart/vegeta/lib"
 )
 
+const RATE_PER_SECOND = 100
+const DURATION_SECONDS = 4
+
 func main() {
-	rate := uint64(100) // per second
-	duration := 4 * time.Second
+
+	rate := uint64(RATE_PER_SECOND) // per second
+	duration := DURATION_SECONDS * time.Second
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL:    "http://web:8000/api/v1/similar?word=apple",
 	})
 	attacker := vegeta.NewAttacker()
-
+	fmt.Printf("duration(s):%d, rate(s):%d \n\n", DURATION_SECONDS, RATE_PER_SECOND)
 	var metrics vegeta.Metrics
 	for res := range attacker.Attack(targeter, rate, duration) {
 		metrics.Add(res)
 	}
 	metrics.Close()
-
-	fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+	json, _ := json.Marshal(metrics)
+	fmt.Printf("metrics:\n %s\n", string(json))
 }
